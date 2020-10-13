efd829c8b903efbffb327e5b04477d48b0fc8d3d
Leandro Picoli <leandropicoli@live.com>
2020-10-13 08:11:44 -0300

    Authenticating User

--- src/app/pages/account/login/login.page.ts
+++ src/app/pages/account/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { LoadingController, NavController, ToastController } from '@ionic/angular';
 import { DataService } from 'src/app/data.service';
+import { UserModel } from 'src/app/models/user.model';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -15,9 +16,7 @@ export class LoginPage implements OnInit {
     private loadingCtrl: LoadingController,
     private toastCtrl: ToastController,
     private navCtrl: NavController,
-    private service: DataService,) { }
-
-  ngOnInit() {
+    private service: DataService,) {
     this.form = this.fb.group({
       username: ['', Validators.compose([
         Validators.required,
@@ -30,8 +29,40 @@ export class LoginPage implements OnInit {
     });
   }
 
+  async submit() {
+    if (this.form.invalid)
+      return;
+
+    const loading = await this.loadingCtrl.create({ message: 'Autenticando...' });
+    loading.present();
+
+    this
+      .service
+      .authenticate(this.form.value)
+      .subscribe(
+        (res: UserModel) => {
+          //SecurityUtil.set(res);
+          loading.dismiss();
+          this.navCtrl.navigateRoot('/');
+        },
+        (err) => {
+          this.showError('Usuário ou senha inválidos');
+          loading.dismiss();
+        }
+      )
+  }
+
+  ngOnInit() {
+  }
+
   toggleHide() {
     this.hide = !this.hide;
+  }
+
+  async showError(message) {
+    //TODO: Close Button
+    const error = await this.toastCtrl.create({ message: message, duration: 3000 });
+    error.present();
   }
 
 }
