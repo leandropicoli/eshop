588bde68c961ba556cb6c3b6540a809c4656b6d1
Leandro Picoli <leandropicoli@live.com>
2020-10-17 10:51:58 -0300

    Initialized OrderDetails

--- src/app/pages/store/order-details/order-details.page.ts
+++ src/app/pages/store/order-details/order-details.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from 'src/app/data.service';
+import { SecurityUtil } from 'src/app/utils/security.util';
+import { StatusUtil } from 'src/app/utils/status.util';
 
 @Component({
   selector: 'app-order-details',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-details.page.scss'],
 })
 export class OrderDetailsPage implements OnInit {
+  public order = null;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: DataService
+  ) { }
 
   ngOnInit() {
+    let number = this.route.snapshot.paramMap.get('number');
+    this.service
+      .getOrder(number)
+      .subscribe((data) => {
+        this.order = data;
+      });
+  }
+
+  translateOrderStatus(status: string): string {
+    return StatusUtil.convert(status);
+  }
+
+  isManager(): boolean {
+    return SecurityUtil.isInRole('manager');
   }
 
 }
